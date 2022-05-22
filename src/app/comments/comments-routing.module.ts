5447cd30bb5a587dae9c62459433f6930e55e2e4
kniazivna <olenkaknyaz@gmail.com>
2022-05-22 22:38:54 +0300

    hw-3  fix comments component

--- src/app/comments/comments-routing.module.ts
+++ src/app/comments/comments-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import {CommentComponent} from "./comments-components/comment/comment.component";
 import {CommentDetailsComponent} from "./comments-components/comment-details/comment-details.component";
+import {CommentsComponent} from "./comments-components/comments/comments.component";
 
 const routes: Routes = [
 
-  {path: '', component: CommentComponent, children:[
+  {path: '', component: CommentsComponent, children:[
       {path: ':id', component: CommentDetailsComponent}
     ]}
 ];
